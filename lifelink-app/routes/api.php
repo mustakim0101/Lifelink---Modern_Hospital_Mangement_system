961ea7e310fbab2d5bad75ed469f4b798c7e5d27
mustakim0101 <mustakim.official.0101@gmail.com>
2026-03-06 11:00:23 +0600

    feat(auth): install jwt-auth with login/register #3

--- lifelink-app/routes/api.php
+++ lifelink-app/routes/api.php
@@ -1,19 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::middleware('auth:api')->group(function () {
+        Route::get('/me', [AuthController::class, 'me']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/refresh', [AuthController::class, 'refresh']);
+    });
+});
+
+Route::prefix('dev')->group(function () {
+    Route::post('/create-admin', [AuthController::class, 'createAdmin']);
 });
